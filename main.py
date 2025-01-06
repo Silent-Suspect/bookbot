17a08fedba9f20da6ede3dd469e1d373c67629b2
Silent-Suspect <Sascha.Mueller@rub.de>
2025-01-06 19:51:29 +0100

    letters sorted by number of occurance

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    #num_words = get_num_words(text)
-    #print(f"{num_words} words found in the document")
-    text_cut = text[:100]
+    num_words = get_num_words(text)
     letter_dict = get_num_characters(text)
-    print(letter_dict)
+    chars_sorted_list = chars_dict_to_sorted_list(letter_dict)
+
+
+    print(f"--- Beginn report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 
 def get_num_words(text):
     words = text.split()
     return len(words)
-
-
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
 
 def get_num_characters(text):
     letters = {} 
@@ -26,8 +31,20 @@ def get_num_characters(text):
         else:
             letters[lowered] = 1
     return letters
-        
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
